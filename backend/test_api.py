71e5279bbcffbdcbfc77bc60736d7ac0e649cd7e
Rahul Kumar <rahulkumarmahto2024@gmail.com>
2026-05-28 21:24:34 +0530

    Update test_api.py

--- backend/test_api.py
+++ backend/test_api.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-import requests
 
 app = FastAPI()
-
-API_KEY = "sk-or-v1- d7bbb3126122552ede45dafcc8215a9995c90 fd2439c441e670e767156479e65""
 
 @app.get("/")
 async def root():
@@ -15,42 +12,6 @@ async def root():
 @app.get("/chat")
 async def chat(message: str):
 
-    response = requests.post(
-
-        "https://openrouter.ai/api/v1/chat/completions",
-
-        headers={
-            "Authorization": f"Bearer {API_KEY}",
-            "Content-Type": "application/json"
-        },
-
-        json={
-
-            "model": "openai/gpt-4.1-mini",
-
-            "messages": [
-                {
-                    "role": "user",
-                    "content": message
-                }
-            ],
-
-            "max_tokens": 200
-        }
-    )
-
-    data = response.json()
-
-    print(data)
-
-    if "choices" in data:
-
-        reply = data["choices"][0]["message"]["content"]
-
-    else:
-
-        reply = str(data)
-
     return {
-        "reply": reply
+        "reply": "Hello " + message
     }
